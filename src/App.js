8b8259660c98cd6b559629ae5fa22908b6dcb38b
William Pscheidt <williampscheidt@hotmail.com>
2022-06-23 13:58:13 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Container from "./components/container";
 import Navbar from "./components/navbar";
 
 import "./App.css"
@@ -8,7 +7,6 @@ function App() {
     <div className="App">
       <header>
         <Navbar />
-        <Container />
       </header>
     </div>
   );
