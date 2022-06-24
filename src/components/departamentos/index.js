10acb3cdd9159db79623a5ae5f247e08f26f174c
William Pscheidt <williampscheidt@hotmail.com>
2022-06-23 23:26:48 -0300

    master

--- src/components/departamentos/index.js
+++ src/components/departamentos/index.js
@@ -6,6 +6,8 @@ import ImgVideogames from '../../assets/Images/Videogames.png'
 import ImgVestuario from '../../assets/Images/Vestuario.png'
 import ImgEsportes from '../../assets/Images/Esportes.png'
 
+import IconArrow from '../../assets/Icons/arrow-right.svg'
+
 const Departamentos = () => {
   return (
     <div className="departamentos">
@@ -13,26 +15,53 @@ const Departamentos = () => {
             <div className='div-destaque'>
                 <span>Departamentos em Destaque</span>
                 <div className='barra-inferior'></div>
+                <div className='infos-card'>
+
+                </div>
             </div>
             <div className='departamentos-cards'>
                 <div className='departamento-card'>
                   <div className='black-effect'></div>
                   <img className='imagem' src={ImgEletronicos} />
+                  <div className='infos-container'>
+                    <div className='infos-card'>
+                      <span className='info-texto'>Eletrônicos</span>
+                    </div>
+                    <img className='icon-arrow' src={IconArrow} />
+                  </div>
                 </div>
 
                 <div className='departamento-card'>
                   <div className='black-effect'></div>
                   <img className='imagem' src={ImgVideogames} />
+                  <div className='infos-container'>
+                    <div className='infos-card'>
+                      <span className='info-texto'>Videogames</span>
+                    </div>
+                    <img className='icon-arrow' src={IconArrow} />
+                  </div>
                 </div>
 
                 <div className='departamento-card'>
                   <div className='black-effect'></div>
                   <img className='imagem' src={ImgVestuario} />
+                  <div className='infos-container'>
+                    <div className='infos-card'>
+                      <span className='info-texto'>Moda & Vestuário</span>
+                    </div>
+                    <img className='icon-arrow' src={IconArrow} />
+                  </div>
                 </div>
 
                 <div className='departamento-card'>
                   <div className='black-effect'></div>
                   <img className='imagem' src={ImgEsportes} />
+                  <div className='infos-container'>
+                    <div className='infos-card'>
+                      <span className='info-texto'>Esportes</span>
+                    </div>
+                    <img className='icon-arrow' src={IconArrow} />
+                  </div>
                 </div>
             </div>
         </div>
