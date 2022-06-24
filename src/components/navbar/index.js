453e58537a5db6d298d5d05fdb48214b5aeded4e
William Pscheidt <williampscheidt@hotmail.com>
2022-06-23 22:36:46 -0300

    master

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import "./navbar.css"
 
+import IconMap from '../../assets/Icons/map-pin.svg'
+import ArrowDown from '../../assets/Icons/arrow-down.svg'
+
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='navbar-container'>
         <div className='navbar-l'>
           <h3 className='logo'>Loja.com.br</h3>
-          <input></input>
-          <span>Selecione o <b>Endereço</b></span>
+          <input className='input-search'></input>
+          <div className='endereco'>
+            <img className="icon-map" src={IconMap} />
+            <span>Selecione o <b>Endereço</b></span>
+            <img className="icon-arrow" src={ArrowDown} />
+          </div>
         </div>
         <div className='navbar-r'>
           <span>Registrar Conta</span>
