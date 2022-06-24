b04fafa70c7ae097328e8a665268345d1f64e989
William Pscheidt <williampscheidt@hotmail.com>
2022-06-23 23:10:30 -0300

    master

--- src/components/departamentos/index.js
+++ src/components/departamentos/index.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import "./departamentos.css"
 
+import ImgEletronicos from '../../assets/Images/Eletronicos.png'
+import ImgVideogames from '../../assets/Images/Videogames.png'
+import ImgVestuario from '../../assets/Images/Vestuario.png'
+import ImgEsportes from '../../assets/Images/Esportes.png'
+
 const Departamentos = () => {
   return (
     <div className="departamentos">
         <div className="departamentos-container">
-            <div>
-                Departamentos em Destaque
+            <div className='div-destaque'>
+                <span>Departamentos em Destaque</span>
+                <div className='barra-inferior'></div>
             </div>
             <div className='departamentos-cards'>
-                <div className='departamento-card'></div>
-                <div className='departamento-card'></div>
-                <div className='departamento-card'></div>
-                <div className='departamento-card'></div>
+                <div className='departamento-card'>
+                  <div className='black-effect'></div>
+                  <img className='imagem' src={ImgEletronicos} />
+                </div>
+
+                <div className='departamento-card'>
+                  <div className='black-effect'></div>
+                  <img className='imagem' src={ImgVideogames} />
+                </div>
+
+                <div className='departamento-card'>
+                  <div className='black-effect'></div>
+                  <img className='imagem' src={ImgVestuario} />
+                </div>
+
+                <div className='departamento-card'>
+                  <div className='black-effect'></div>
+                  <img className='imagem' src={ImgEsportes} />
+                </div>
             </div>
         </div>
     </div>
