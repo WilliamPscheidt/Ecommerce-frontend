c96333cc698c9455daef4ba2d390c4d3426cfadb
William Pscheidt <williampscheidt@hotmail.com>
2022-06-23 22:50:57 -0300

    master

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import "./navigation.css"
 
+import IconMenu from '../../assets/Icons/align-left.svg'
+
 const Navigation = () => {
   return (
     <div className="navigation">
         <div className="navigation-container">
             <div className="navigation-l">
-                <span>Todos os Departamentos |</span>
-                <span>Mais Vendidos |</span>
-                <span>Ofertas do Dia |</span>
-                <span>Música |</span>
-                <span>Livros |</span>
-                <span>Atendimento ao Cliente |</span>
-                <span>Eletrônicos</span>
+                <img className="icon-menu" src={IconMenu} />
+                <span className='texto-navigation'>Todos os Departamentos</span>
+                <span className='separator'>|</span>
+                <span className='texto-navigation'>Mais Vendidos</span>
+                <span className='separator'>|</span>
+                <span className='texto-navigation'>Ofertas do Dia</span>
+                <span className='separator'>|</span>
+                <span className='texto-navigation'>Música</span>
+                <span className='separator'>|</span>
+                <span className='texto-navigation'>Livros</span>
+                <span className='separator'>|</span>
+                <span className='texto-navigation'>Atendimento ao Cliente</span>
+                <span className='separator'>|</span>
+                <span className='texto-navigation'>Eletrônicos</span>
             </div>
             <div className="navigation-r">
                 <span>Carrinho</span>
