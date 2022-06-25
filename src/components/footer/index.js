4497efce3dafd121d8d691167048789e2802ed0e
William Pscheidt <williampscheidt@hotmail.com>
2022-06-25 02:14:59 -0300

    master

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,33 +1,35 @@
 import React from 'react'
 import './footer.css'
 
+import IconArrowUp from '../../assets/Icons/arrow-up.svg'
+
 const Footer = () => {
   return (
     <div className='footer-container'>
         <div className='footer-internal'>
             <div className='logo'>
-                <span>Loja.com.br</span>
-                <span>@WilliamPscheidt</span>
+                <span className='logo-texto'>Loja.com.br</span>
+                <span className='logo-texto'>@WilliamPscheidt</span>
             </div>
             <div className='navegacao'>
-                <ul>
-                    <li>Mais Vendidos</li>
-                    <li>Ofertas do Dia</li>
-                    <li>Música</li>
+                <ul className='links'>
+                    <li className='link'>Mais Vendidos</li>
+                    <li className='link'>Ofertas do Dia</li>
+                    <li className='link'>Música</li>
                 </ul>
-                <ul>
-                    <li>Música</li>
-                    <li>Livros</li>
-                    <li>Computadores</li>
+                <ul className='links'>
+                    <li className='link'>Música</li>
+                    <li className='link'>Livros</li>
+                    <li className='link'>Computadores</li>
                 </ul>
-                <ul>
-                    <li>Atendimento ao Cliente</li>
-                    <li>Acessar minha Conta</li>
-                    <li>Meus Pedidos</li>
+                <ul className='links'>
+                    <li className='link'>Atendimento ao Cliente</li>
+                    <li className='link'>Acessar minha Conta</li>
+                    <li className='link'>Meus Pedidos</li>
                 </ul>
             </div>
             <div className='voltar'>
-                Voltar
+                <img src={IconArrowUp} />
             </div>
         </div>
     </div>
