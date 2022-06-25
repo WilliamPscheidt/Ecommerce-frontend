710a00b28339e44f60e2b6b397455d7edd89ccda
William Pscheidt <williampscheidt@hotmail.com>
2022-06-24 23:27:25 -0300

    master

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,22 +1,18 @@
 import React from 'react'
 import './slider.css'
 
-import Slider1 from '../../assets/Images/Slider1.png'
-
 import IconLeft from '../../assets/Icons/arrow-left.svg'
 import IconLeft2 from '../../assets/Icons/arrow-left2.svg'
 
 const Slider = () => {
   return (
     <div className='slider'>
-        <div className='container-central'>
-            <div className='container-l'>
-                <img src={IconLeft} />
-            </div>
-            <div className='container-r'>
-                <img src={IconLeft2} />
-            </div>
-        </div>
+      <div className='container-l'>
+        <img src={IconLeft} />
+      </div>
+      <div className='container-r'>
+        <img src={IconLeft2} />
+      </div>
     </div>
   )
 }
